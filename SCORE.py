ed036c46d388adf6bf4492af1e284983eb492574
holiday01 <zx12as3420@gmail.com>
2019-06-28 13:46:43 +0800

    added a new feature some files changed

---
+++ SCORE.py
@@ -0,0 +1,40 @@
+import pandas as pd
+import numpy as np
+from sklearn.svm import LinearSVR
+from sklearn.datasets import make_regression
+import matplotlib.pyplot as plt
+import random
+from matplotlib.pyplot import figure
+test_data = pd.read_csv("Sample.csv")
+reference = pd.read_csv("Reference.csv")
+train = reference.drop("Gene",1)
+test_data = test_data.drop("gene",1)
+
+score_adj = []
+for o in range(len(test_data.columns)):
+    test = test_data.loc[:,test_data.columns[o]]
+    im_name = train.columns
+    svr = LinearSVR(random_state=0)
+    model = svr.fit(train, test)
+    score = model.coef_
+    score[np.where(score<0)] = 0 
+    score_adj.append((score/sum(score)))
+score_adj = pd.DataFrame(score_adj)
+score_adj.columns = im_name
+score_adj.plot(kind='bar', stacked=True,legend=False)
+plt.legend(loc='center left', bbox_to_anchor=(1.0, 0.5))
+plt.suptitle("Flow&Estimate")
+plt.rcParams['figure.figsize'] = (6.69,8.86)
+plt.rcParams['figure.dpi'] = 300
+name = "4.bar.pdf"
+plt.savefig(name,bbox_inches="tight" )
+plt.close()
+plt.boxplot(score_adj.T,patch_artist = True)
+plt.suptitle("Flow&Estimate")
+plt.xticks(range(8), im_name)
+plt.rcParams['figure.figsize'] = (6.69,8.86)
+plt.rcParams['figure.dpi'] = 300
+name = "5.boxplot.pdf"
+plt.savefig(name)
+
+score_adj.to_csv("score.csv")
---
+++ pre-process.R
@@ -0,0 +1,27 @@
+## sample data
+sample <- read.delim("input.data")
+length(unique(sample[,1]))
+
+h <- apply(sample[,-1], 1, mean)
+names(h) <- seq(1,length(sample[,1]),1)
+hc <- {}
+for(n in 1:length(as.character(unique(sample[which(duplicated(sample[,1])=="TRUE"),1])))){
+  g1 <- h[which(as.character(sample[,1])==as.character(unique(sample[which(duplicated(sample[,1])=="TRUE"),1]))[n])]
+  hc[n] <- names(which(g1==max(g1)))
+}
+sample_fi <- sample[-which(sample[,1]%in%sample[hc,1]),]
+length(unique(sample_fi[,1]))
+
+sample_fi <- rbind(sample[hc,],sample_fi)
+length(unique(sample_fi[,1]))
+
+MF <- read.csv("Reference.csv")
+## select genes both in sample matrix and reference matrix ====
+im_ref <- MF[which(MF[,1]%in%sample_fi[,1]),]
+sample_ref <- sample_fi[which(sample_fi[,1]%in%im_ref[,1]),]
+
+im_ref <- im_ref[order(im_ref[,1]),]
+sample_ref <- sample_ref[order(sample_ref[,1]),]
+write.csv(im_ref,"Reference.csv",row.names = F)
+write.csv(sample_ref,"Sample.csv",row.names = F)
+
